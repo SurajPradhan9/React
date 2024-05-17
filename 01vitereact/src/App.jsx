d23f122a01db5fe9db7521f4b9d03c5cb03de1fd
Suraj Pradhan <surajpradhan@9104@gmail.com>
2024-05-17 14:17:20 +0530

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,9 @@
 import Chai from "./chai"
 function App() {
-
+  const username = "Bhai"
   return (
     <>
-    <h1>Chai aur react with vite</h1>
+    <h1>Chai aur react with vite {username}</h1>
     <Chai/>
     <div>Hy Surajj..</div>
     </>
