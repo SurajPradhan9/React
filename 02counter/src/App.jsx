b9968cbb54b2e9c87c29f7a6ab540261f32e0fd9
Suraj Pradhan <surajpradhan@9104@gmail.com>
2024-05-18 08:41:21 +0530

    interview question on counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -7,13 +7,26 @@ function App() {
 
   const [counter, setCounter]  = useState(0)
 
-  //let counter = 15
-
   const addValue = () => {
-    //counter = counter + 1
-    if(counter<20){
-      setCounter(counter+1)
-    }
+    // if(counter<20){
+    //   setCounter(counter+1)
+    // }
+
+    // //the above 4 lines  increment the value only once because due to useState they are sent in batches to ui so only one increment occurs (fiber concept)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+
+    //so if we want to do the above task as we are wanting to do ,do like this
+    //as we know setCounter is a function it accepts a callback function, here in below we are passing parameter name prevCounter(gives the existing state) which is the last updated state or previous value of counter(meaning:we fetched the value of previous updated state and passed to the callback function), so in this context after clicking we increment the value by 4
+    setCounter((prevCounter)=>prevCounter+1)
+    setCounter((prevCounter)=>prevCounter+1)
+    setCounter((prevCounter)=>prevCounter+1)
+    setCounter((prevCounter)=>prevCounter+1)
+
+    //now they are not a bunch of packages that  are passed once, we are taking the previous state as reference so they are sent individually
+
   }
 
   const removeValue = () => {
